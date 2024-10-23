0cf0e7c45e05dc382857d3e35e02f03f0affbe83
wilville <racecarwil@gmail.com>
2024-10-22 23:07:09 -0300

    lel

---
+++ hypr/.config/hypr/bin/music.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+set url (playerctl metadata mpris:artUrl)
+
+if test -z $url
+    echo ""
+else if string match -q --regex "^file://" $url
+    echo (string replace --regex "^file://" '' $url)
+else
+    set path /tmp/hyprlock/(basename $url)
+    if not test -e $path
+        curl -s $url -o "$path"
+        magick $path "$path".png
+    end
+    echo "$path".png
+end
